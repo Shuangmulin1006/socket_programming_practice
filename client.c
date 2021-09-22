61668dd11c1e5ffaf3114c5349b09089db9724a3
Aaron <b10202227@gapps.ntust.edu.tw>
2021-09-22 17:13:53 +0800

    Add tcp server and client

---
+++ client.c
@@ -0,0 +1,50 @@
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#define PORT 8080
+
+int main(int argc, char const *argv[])
+{
+	int sock = 0, valread;
+	struct sockaddr_in serv_addr;
+	unsigned char send_buf[1024] = {0};
+	char buffer[1024] = {0};
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+	{
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	{
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+
+	while (1) {
+		if (fgets(send_buf, 1023, stdin)) {
+			if (send(sock, send_buf, strlen(send_buf), 0) <= 0) {
+				perror("Send");
+				close(sock);
+				return -1;
+			}
+			recv(sock, buffer, 1024, 0);
+			printf("%s\n", buffer);
+		}
+	}
+	close(sock);
+	return 0;
+}
